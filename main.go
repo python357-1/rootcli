c7564510ddf664a08863deb034e5ba2dc72588b6
Jordan51104 <jb2101554@gmail.com>
2023-10-03 21:37:39 -0500

    test commit 3

--- main.go
+++ main.go
@@ -12,9 +12,13 @@ func bounds(target float64, accuracy float64) (float64, float64) {
 
 func main() {
 	operand := flag.Float64("o", 0, "a non-negative number whose root is to be found")
+	root := flag.Int("rt", 1, "a non-negative integer that is the root value (2 = square root, 3 = cube root, etc) ")
 	accuracy := flag.Float64("acc", 1, "a non-negative number which describes how far away the found root may be from the actual root")
+	flag.Parse()
 
+	fmt.Printf("%f, %d, %f\n", *operand, *root, *accuracy)
 	var operandFixed float64
+	foundRoot := 1.0
 	if *operand == 0 {
 		fmt.Println(0)
 		return
@@ -25,7 +29,6 @@ func main() {
 		operandFixed = *operand
 	}
 	lowerBound, higherBound := bounds(operandFixed, *accuracy)
-	for {
 		exponentiation := math.Pow(foundRoot, float64(*root))
 		fmt.Printf("%f ** %f == %f\n", foundRoot, float64(*root), exponentiation)
 		if exponentiation < higherBound && exponentiation > lowerBound {
